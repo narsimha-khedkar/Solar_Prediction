4cca1c6d7ccc872c3566bdf773ab83d05b5a7e4e
Thomas James Barnhouse <barnhouset09@gmail.com>
2021-03-13 16:40:13 -0500

    Updated to use sln file

--- PythonApp/PythonApp.py
+++ PythonApp/PythonApp.py
@@ -1,8 +1,1 @@
-%matplotlib inline
-import pandas as pd
-import numpy as np
-import sys, os
-from IPython.display import display
-from geopy.geocoders import Nominatim
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
+
