450be69e5d03429b83e1ebfd6daa41cdae275eb0
Thomas James Barnhouse <barnhouset09@gmail.com>
2021-03-22 13:36:49 -0400

    # Publish Profile Added - cleaned up branding

--- AngularApp/ClientApp/src/app/app.module.ts
+++ AngularApp/ClientApp/src/app/app.module.ts
@@ -1,31 +1,34 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
 
+import { AppRoutingModule } from "./app.routing";
+import { ComponentsModule } from "./components/components.module";
 
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
+import { AppComponent } from "./app.component";
 
-import { AppComponent } from './app.component';
-
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { TableListComponent } from "./table-list/table-list.component";
+import { TypographyComponent } from "./typography/typography.component";
+import { IconsComponent } from "./icons/icons.component";
+import { MapsComponent } from "./maps/maps.component";
+import { NotificationsComponent } from "./notifications/notifications.component";
+import { UpgradeComponent } from "./upgrade/upgrade.component";
+import { AgmCoreModule } from "@agm/core";
+import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
 import {
-  AgmCoreModule
-} from '@agm/core';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+  NgxGoogleAnalyticsModule,
+  NgxGoogleAnalyticsRouterModule,
+} from "ngx-google-analytics";
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
+    NgxGoogleAnalyticsModule.forRoot("G-W3T8E40EV7"),
+    NgxGoogleAnalyticsRouterModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
@@ -33,15 +36,11 @@ import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.compon
     RouterModule,
     AppRoutingModule,
     AgmCoreModule.forRoot({
-      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
-    })
+      apiKey: "YOUR_GOOGLE_MAPS_API_KEY",
+    }),
   ],
-  declarations: [
-    AppComponent,
-    AdminLayoutComponent,
-
-  ],
+  declarations: [AppComponent, AdminLayoutComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
