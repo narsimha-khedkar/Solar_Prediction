5918bc0a214ff799cf507c8ccd7c0a5ee6b87679
Thomas James Barnhouse <barnhouset09@gmail.com>
2021-03-24 13:17:22 -0400

    # Python API Configured - Added a simple example API, will need to migrate some code from Anthony and Narasimha's ML work - Added swagger doc so API layer can be tested more readily

--- PythonApp/PythonApp.py
+++ PythonApp/PythonApp.py
@@ -1,2 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
-#test
+@app.route('/api/docs')
+def get_docs():
+    print('sending docs')
+    return render_template('swaggerui.html')
+
+@app.route('/greetings')
+def greetings():
+    return 'May the force be with you!'
+
+if __name__ == '__main__':
+    app.run()
